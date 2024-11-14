74318298c700874364fec3af02d9cb765440f6ba
Gabrielllloo <gabrielvicenteorallo@gmail.com>
2024-11-14 13:57:06 -0500

    Update ohayo.py

--- ohayo.py
+++ ohayo.py
@@ -1,3 +1,9 @@
-print("DrowningYu777 is a great book which written by DOL in 2019")
-print("One book is very cheap which cost 1000 dollars")
-print()
+print("This is my bookstore. Welcome!!")
+import random
+
+choice = input('Enter a choice (DrowningYu777, Fakemaker788):')
+p_choice = ['rock','paper','scissors']
+c_choice = random.choice(p_choice)
+print('You choose',(choice), ',computer choose', (c_choice))
+print("The first book called <<DrowningYu777>>, a greak book which was written by dolores in 2019\nAnd it's very cheap which only cost 1000 dollars per book")
+print("The second book is <<Fakemaker788>>, a famous author Caden published it in 2000. The present price is setted in 40 dollars per book.")
