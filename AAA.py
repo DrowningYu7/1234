3bf765d8eac7192d853a4895f2cbd3cfe658ff0c
Gabrielllloo <gabrielvicenteorallo@gmail.com>
2024-11-18 14:21:39 -0500

    1

---
+++ AAA.py
@@ -0,0 +1,3 @@
+class AAA():
+    def Add():
+        input
--- ohayo.py
+++ ohayo.py
@@ -2,8 +2,16 @@ print("This is my bookstore. Welcome!!")
 import random
 
 choice = input('Enter a choice (DrowningYu777, Fakemaker788):')
-p_choice = ['rock','paper','scissors']
-c_choice = random.choice(p_choice)
-print('You choose',(choice), ',computer choose', (c_choice))
-print("The first book called <<DrowningYu777>>, a greak book which was written by dolores in 2019\nAnd it's very cheap which only cost 1000 dollars per book")
+print("The first book is called <<DrowningYu777>>, a greak book which was written by dolores in 2019\nAnd it's very cheap which only cost 1000 dollars per book")
 print("The second book is <<Fakemaker788>>, a famous author Caden published it in 2000. The present price is setted in 40 dollars per book.")
+print("The third book is called <<XiaoMing707>>, it was writen by an actress CiCi. She set the price at 500 dollars.")
+print("The forth book is <<KILL YOURSELF>> which written by Shawn in 2024. 200 dollars per book")
+
+print("Choose the book you want")
+print("1.DrowningYu777\n2.Fakemaker788\n3. ")
+choice = input('Enter a choice (DrowningYu777, Fakemaker788):')
+
+
+
+
+print("1.Add books\n2.Sell books\n3.Increase the price\n4.Reduce the price")
