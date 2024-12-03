aef23c99464c725270b987967fa43bd40fcea4e0
Gabrielllloo <gabrielvicenteorallo@gmail.com>
2024-12-03 14:20:43 -0500

    1

--- AAA.py
+++ AAA.py
@@ -21,11 +21,11 @@ class Bookstore():
 
     def Sell_books(self):
         self._Name=input("Enter the name of the book:")
-        self._Author=input("Enter the author of the book:")
-        self._Price=input("Enter the price of the book:")
+        
 
         if self._Name in self._Books:
             print(self._Books)
+            del self.Books[variable]
             print("\nYou sold this book successfully!")
         else:
             print("\nSorry.Book not found in the Bookstore.")    
