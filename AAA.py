d308a89aa10acf59888ca65f85881a030b611d5f
Gabrielllloo <gabrielvicenteorallo@gmail.com>
2024-12-09 15:05:48 -0500

    1

--- AAA.py
+++ AAA.py
@@ -10,8 +10,7 @@ class Bookstore():
     def Add_books(self):
         self._Name = input("Enter the name of the book:")
         self._Author = input("Enter the author of the book:")
-        self._Price = input("Enter the price of the book:")
-
+        self._Price = float(input("Enter the price of the book:"))
         self._Books[self._Name] = {"Author":self._Author,"Price":self._Price}
         print("\nYou added this book successfully!")
         print("Now you have these books")
@@ -21,7 +20,6 @@ class Bookstore():
         
     def Sell_books(self):
         self._Name=input("Enter the name of the book:")
-        
 
         if self._Name in self._Books:
             print(self._Books)
@@ -29,18 +27,28 @@ class Bookstore():
             print("\nYou sold this book successfully!")
         else:
             print("\nSorry TvT Book not found in the Bookstore.")    
-        print("Now you have these books")
+        print("Now you have these books:")
         print(self._Books)
         print("\n")
         
 
     def Show_all_books(self):
+        # for s in self._Books:
+        #     print(self._Books[s]["Author"])
+        #     print(self._Books[s]["Price"])
+        #     print("\n")
         for s in self._Books:
-            print(s,self._Books[s]["Author"])
-            print(self._Books[s]["Price"])
+            print(f"Book: {s}, Author: {self._Books[s]['Author']}, Price: ${self._Books[s]['Price']:.2f}")
             print("\n")
             
     def Increase_price(self):
         self._book_Name = input("Enter the name of the book that you want to increase the price:")
         if self._book_Name in self._Books:
             print 
+
+
+    def Save_books_to_file(self, filename="booklist.txt"):
+         with open(filename, "w") as file:
+             for name, info in self._Books.items():
+                    file.write(f"Book: {name}, Author: {info['Author']}, Price: ${info['Price']:.2f}\n")
+             print(f"\nBooks have been saved to {filename} successfully!\n")
--- ohayo.py
+++ ohayo.py
@@ -3,7 +3,7 @@ Variable = Bookstore()
 while True:
  
     print("Welcome to my bookstore")
-    print("1.Add books\n2.Sell books\n3.Increase the price\n4.Reduce the price\n5.Show all books\n6.Quit")
+    print("1.Add books\n2.Sell books\n3.Increase the price\n4.Reduce the price\n5.Show all books\n6.Save books to the file\n7.Quit\n")
 
     choice = input("Enter your choice:")
     if choice =="1":
@@ -17,9 +17,11 @@ while True:
     elif choice =="5":
       show = Variable.Show_all_books()
     elif choice =="6":
+      save = Variable.Save_books_to_file()
+    elif choice =="7":
       print("Exit the Bookstore")
     else:
       print("An invalid choice, please try again")
       break
-
+    
 
